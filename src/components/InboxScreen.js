199d57479d6f0141d5759deca600e9cdb00d24a7
EthanColin <ethan_colinlaw@hotmail.com>
2023-01-18 09:59:46 +0800

    added workflow folder

--- src/components/InboxScreen.js
+++ src/components/InboxScreen.js
@@ -16,7 +16,7 @@ export default function InboxScreen() {
 		return (
 			<div className="page lists-show">
 				<div className="wrapper-message">
-					<span className="icon-face-sad" />
+					<span className="icon-face-happy" />
 					<p className="title-message">Oh no!</p>
 					<p className="subtitle-message">Something went wrong</p>
 				</div>
